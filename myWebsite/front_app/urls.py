d87c4f8dbdca46730fec221f4614f226f636697b
Jean-Baptiste JOLY <jbjoly@octo.com>
2019-12-13 19:06:26 +0100

    feat: add view, url and template for vegetable

--- myWebsite/front_app/urls.py
+++ myWebsite/front_app/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^home', views.home, name='home'),
     url(r'^fruit/(?P<fruit>\w+)', views.fruit, name='fruit'),
+    url(r'^vegetable/(?P<vegetable>\w+)', views.vegetable, name='vegetable'),
     url(r'', views.home, name='home'),
 ]
