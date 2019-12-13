dac89f3cb718497bf4eb0549a5eb27e6a581195f
Jean-Baptiste JOLY <jbjoly@octo.com>
2019-12-13 18:43:22 +0100

    feat: add first view with templating

--- myWebsite/front_app/urls.py
+++ myWebsite/front_app/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'', views.home, name='home'), #
     url(r'^home', views.home, name='home'),
+    url(r'^fruit/(?P<fruit>\w+)', views.fruit, name='fruit'),
+    url(r'', views.home, name='home'),
 ]
--- myWebsite/front_app/views.py
+++ myWebsite/front_app/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from django.shortcuts import render
 
 
 def home(request):
@@ -14,3 +13,12 @@ def home(request):
         <h1>Prix journalier des fruits et légumes</h1>
         <p>Alors combien ça coûte ?</p>
     """)
+
+def fruit(request, fruit):
+    return render(request, 'front_app/fruit.html', {'fruit': fruit})
+#
+# def fruit(request):
+#     HttpResponse("""
+#         <p>fruit 1</p>
+#         <p>fruit 2</p>
+#     """)
